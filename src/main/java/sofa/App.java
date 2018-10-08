5b663fee5fbe542ebd01ccd0b77265ccea8b5986
maxnocker <max.nocker@gmail.com>
2018-10-08 15:07:12 +0200

    Initial commit

---
+++ src/main/java/sofa/App.java
@@ -0,0 +1,20 @@
+package sofa;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static int add(int a, int b) {
+    	return a+b;
+    }
+    
+    public static int mult(int a, int b) {
+    	return a*b;
+    }
+}
